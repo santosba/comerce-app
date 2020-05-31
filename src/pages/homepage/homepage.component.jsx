2572185391298ebffcd8453eacb62fc3a2f42d62
santosba <gustavoba8@gmail.com>
2020-05-31 13:08:48 +0000

    2-homepage folder and menuitem and directory folder

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './homepage.style.scss';
+import Directory from '../../componensts/directory/directory.component';
+
+const HomePage = () => (
+  <div className="home-page">
+ <Directory/>
+     
+  </div>
+);
+
+export default HomePage;
